0f7a1fc77e4c671ae830eb1ef02133261e4e7aa8
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-10-13 10:58:24 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -133,7 +133,7 @@ def new_member(bot: Bot, update: Update):
         for new_mem in new_members:
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("Master is in the houseeee, let's get this party started!")
+                update.effective_message.reply_text("Master is in the houseeee, let's get this party started🥳")
                 continue
 
             # Don't welcome yourself
